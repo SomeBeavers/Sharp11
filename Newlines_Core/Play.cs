b3eb95843637644719f275ed857b0e10c7ae9991
lilia shamsutdinova <lilia.shamsutdinova@jetbrains.com>
2022-04-13 12:16:43 +0300

    Update Play.

---
+++ Newlines_Core/Play.cs
@@ -0,0 +1,11 @@
+﻿using Newlines_Core;
+
+namespace MyNamespace;
+
+class Play
+{
+    string log = $"{Name +
+    new List<InnerClass>()
+        .Where(x => x.Name == "Name")}";
+    public static string Name { get; set; }
+}
