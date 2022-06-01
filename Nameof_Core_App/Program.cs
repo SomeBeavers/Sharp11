9a4a97bbb9c82fd76e12da33d69ff434a1b3b944
lilia shamsutdinova <lilia.shamsutdinova@jetbrains.com>
2022-06-01 10:22:10 +0300

    Add nameof(parameter)

---
+++ Nameof_Core_App/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+[MyAttribute(nameof(parameter))]
+void Method1(string parameter)
+{
+}
+
+public class MyAttribute : Attribute
+{
+    public MyAttribute(string parameter1)
+    {
+    }
+}
