d58cae4fe3b5972ac897d410926f6918dbe682d3
lilia shamsutdinova <lilia.shamsutdinova@jetbrains.com>
2022-07-18 15:47:00 +0200

    update.

---
+++ Nameof_Core_App/NameofCompletion.cs
@@ -0,0 +1,13 @@
+﻿namespace Nameof_Core_App;
+
+public class NameofCompletion
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="parameter">test</param>
+    [My(nameof(parameter))]
+    public void Test(string parameter)
+    {
+    }
+}
