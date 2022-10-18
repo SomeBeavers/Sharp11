61c10d9aae56da656f226ef0b98f9ee36b95fbeb
lilia shamsutdinova <lilia.shamsutdinova@jetbrains.com>
2022-10-18 10:54:56 +0200

    File local types: "Code" tests.

---
+++ FileLocalTypes_CoreApp/Code/File.cs
@@ -0,0 +1,24 @@
+﻿namespace FileLocalTypes_CoreApp.Code;
+
+file class @file<T> where T: FileClass, new()
+{
+    public void Test()
+    {
+        T t = new T();
+        //var @int = 1;
+    }
+}
+
+file class FileClass
+{
+
+}
+
+
+public class UseFileClass
+{
+    public void Test()
+    {
+        var file = new file<FileClass>();
+    }
+}
---
+++ FileLocalTypes_CoreApp/Code/TypeInNestedNamespace.cs
@@ -0,0 +1,32 @@
+﻿namespace FileLocalTypes_CoreApp.Code1
+{
+    public class TypeInNestedNamespace
+    {
+    
+    }
+
+    file class DuplicatedClass
+    {
+        
+    }
+
+    namespace MyNamespace
+    {
+        class DuplicatedClass
+        {
+            public DuplicatedClass(string s)
+            {
+            }
+        }
+
+        class UseDuplicatedClass
+        {
+            public void Test()
+            {
+                new DuplicatedClass("");
+                new DuplicatedClass("");
+            }
+        }
+    }
+}
+
