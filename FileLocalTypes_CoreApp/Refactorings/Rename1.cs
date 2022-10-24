3b7c9cb3e67933f6f85a00cb7f644e7513d06235
lilia shamsutdinova <lilia.shamsutdinova@jetbrains.com>
2022-10-24 15:02:10 +0200

    File local types: update.

---
+++ FileLocalTypes_CoreApp/Refactorings/Rename1.cs
@@ -0,0 +1,15 @@
+﻿namespace FileLocalTypes_CoreApp.Refactorings;
+
+file class Rename4
+{
+    
+}
+
+public class UseRename
+{
+    public void Test()
+    {
+        var rename1 = new Rename4();
+        var rename2 = new Rename5();
+    }
+}
---
+++ FileLocalTypes_CoreApp/Refactorings/Rename1_2.cs
@@ -0,0 +1,14 @@
+﻿namespace FileLocalTypes_CoreApp.Refactorings;
+
+public class Rename5
+{
+    
+}
+
+public class UseRenameAnother
+{
+    public void Test()
+    {
+        new Rename5();
+    }
+}
