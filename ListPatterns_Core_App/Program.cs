8774accf69ce6323283d38205ebbdd48ce402f39
lilia shamsutdinova <lilia.shamsutdinova@jetbrains.com>
2022-11-21 12:30:57 +0100

    List patterns: add projects.

---
+++ ListPatterns_Core_App/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+var list = new List<string> { "", "Jane", "John" };
+var b = list is ["", "Jane"];
---
+++ ListPatterns_Framework/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListPatterns_Framework
+{
+    public class Class1
+    {
+        public void Test(int[] array)
+        {
+            if (array is [1,2])
+            {
+                
+            }
+        }
+    }
+}
