beea5356e0d810a614056a6cbb0ef69ffef3087f
lilia shamsutdinova <lilia.shamsutdinova@jetbrains.com>
2022-11-29 17:41:23 +0100

    SAM: update.

---
+++ SAM_Core_App/Code/Implementation.cs
@@ -0,0 +1,140 @@
+﻿namespace SAM_Core_App.Code;
+
+public class Implementation: IImplementationInterface<Implementation>, IImplementationInterface2<Implementation>
+{
+    static Implementation _zero;
+
+    public static Implementation Zero
+    {
+        get => _zero;
+        set => _zero = value;
+    }
+
+    public static void AbstractMethod()
+    {
+    }
+
+    public static string Name { get; set; }
+
+    public static Implementation Addition()
+    {
+        return null;
+    }
+
+}
+
+public interface IImplementationInterface<T> where T : IImplementationInterface<T>
+{
+    public static abstract T Zero { get; set; }
+
+    public static abstract void AbstractMethod();
+
+    public static virtual string Name { get; }
+
+    static virtual T Addition()
+    {
+        return default;
+    }
+}
+
+public record MyRecord: IImplementationInterface<MyRecord>
+{
+    static MyRecord IImplementationInterface<MyRecord>.Zero { get; set; }
+
+    static void IImplementationInterface<MyRecord>.AbstractMethod()
+    {
+    }
+}
+
+public struct MyStruct: IImplementationInterface<MyStruct>
+{
+    public static MyStruct Zero { get; set; }
+    public static void AbstractMethod()
+    {
+    }
+}
+
+public abstract class BaseClass : IImplementationInterface<BaseClass>
+{
+    public static BaseClass Zero { get; set; }
+    public static void AbstractMethod()
+    {
+    }
+}
+
+public class BaseClassImpl : BaseClass
+{
+    public void Test()
+    {
+        var baseClass = BaseClassImpl.Zero;
+    }
+}
+
+public interface IImplementationInterface2<T> where T : IImplementationInterface2<T>, new()
+{
+    public static abstract T Zero { get; set; }
+
+    static virtual T Addition()
+    {
+        return new T();
+    }
+}
+
+public class Use2<T> where T : IImplementationInterface<T>, new()
+{
+    public void Test(Implementation parameter, IDerivedInterface1 parameter2)
+    {
+        var zero = Implementation.Zero;
+        var use2 = T.Zero;
+
+        var derivedInterface1 = IDerivedInterface1.Addition();
+    }
+}
+
+public partial interface IDerivedInterface1 : IImplementationInterface<IDerivedInterface1>
+{
+    static IDerivedInterface1 IImplementationInterface<IDerivedInterface1>.Zero
+    {
+        get => null;
+        set { }
+    }
+
+    static void IImplementationInterface<IDerivedInterface1>.AbstractMethod()
+    {
+    }
+
+    new static IDerivedInterface1 Addition()
+    {
+        return null;
+    }
+
+    public static virtual partial void Test();
+}
+public partial interface IDerivedInterface1 : IImplementationInterface<IDerivedInterface1>
+{
+
+    public static virtual partial void Test(){}
+
+    public static extern virtual void Test2();
+}
+
+public interface IImplementationInterface3
+{
+    static abstract void Test();
+    void Test2();
+}
+
+
+interface I0
+{
+    static sealed void M() => Console.WriteLine("Default behavior");
+    
+    
+    static sealed int P1 { get; set; }
+
+    static sealed event Action E1;
+    static sealed event Action E2 { add => E1 += value; remove => E1 -= value; }
+    
+    static sealed I0 operator +(I0 l, I0 r) => l;
+}
+
--- SAM_Core_App/Code/Operators.cs
+++ SAM_Core_App/Code/Operators.cs
@@ -2,5 +2,89 @@
 
 public class Operators
 {
-    
+}
+
+public class C : IOperators<C>
+{
+    public static void Abstract1()
+    {
+    }
+
+    public static bool operator ==(C left, C right)
+    {
+        return false;
+    }
+
+    public static bool operator !=(C left, C right)
+    {
+        return false;
+    }
+
+    public static C operator ++(C t)
+    {
+        return null;
+    }
+
+    public static C operator checked ++(C t)
+    {
+        return null;
+    }
+}
+
+internal interface IOperators<T> where T : IOperators<T>
+{
+    static abstract void Abstract1();
+    static abstract bool operator ==(T left, T right);
+    static abstract bool operator !=(T left, T right);
+
+    static abstract T operator ++(T t);
+    static abstract T operator checked ++(T t);
+}
+
+interface IDerivedOperators<T>:IOperators<T> where T : IOperators<T>
+{
+    static bool IOperators<T>.operator ==(T left, T right)
+    {
+        return false;
+    }
+
+    static bool IOperators<T>.operator !=(T left, T right)
+    {
+        return false;
+    }
+}
+
+//internal abstract class DerivedOperatorsBase
+//{
+//    public abstract void Abstract1();
+//}
+
+class DerivedOperators : IDerivedOperators<DerivedOperators>
+{
+    public static void Abstract1()
+    {
+    }
+
+    public static DerivedOperators operator ++(DerivedOperators t)
+    {
+        return null;
+    }
+
+    public static DerivedOperators operator checked ++(DerivedOperators t)
+    {
+        return null;
+    }
+}
+
+class Generic1<T> where T : IOperators<T>
+{
+    public void Test(T left, T right)
+    {
+        T.Abstract1();
+        right = left;
+        var t = left++;
+        right = left;
+        var derivedOperators = (t as DerivedOperators);
+        DerivedOperators.Abstract1();
+    }
 }
