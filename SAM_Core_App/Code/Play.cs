a63ef0042d16ec652a5a397a1107e2a51794918b
lilia shamsutdinova <lilia.shamsutdinova@jetbrains.com>
2022-11-30 18:37:06 +0100

    SAM: update.

---
+++ SAM_Core_App/Code/Play.cs
@@ -0,0 +1,51 @@
+﻿namespace SAM_Core_App.Code;
+
+public class Play: IPlay1<Play>, IPlay2<Play>
+{
+    static void IPlay1<Play>.Test()
+    {
+        Test();
+    }
+
+    static void IPlay2<Play>.Test()
+    {
+        Test();
+    }
+
+    public static void Test()
+    {
+        Console.WriteLine("Test");
+    }
+
+    static Play IPlay1<Play>.operator +(Play left, Play right)
+    {
+        return left + right;
+    }
+
+
+    public static Play operator +(Play left, Play right)
+    {
+        return null;
+    }
+}
+
+public interface IPlay1<T> where T : IPlay1<T>
+{
+    static abstract void Test();
+
+
+    static virtual T operator +(T left, T right)
+    {
+        return left + right;
+    }
+}
+public interface IPlay2<T> where T : IPlay2<T>
+{
+
+    static abstract void Test();
+
+    static virtual T operator +(T left, T right)
+    {
+        return left + right;
+    }
+}
--- SAM_Core_App/Code/Usages3.cs
+++ SAM_Core_App/Code/Usages3.cs
@@ -4,8 +4,20 @@
 // interface as generic constraint
 public class Usages3<T> where T : IUsages3<T>
 {
-    public void Test(T parameterT)
+    public void Test(T parameterT, string stringParameter = "")
     {
+        var abstractMethod = T.AbstractMethod();
+        T? virtualMethod = T.VirtualMethod_reabstraction(parameterT);
+        parameterT = stringParameter;
+
+        // ReSharper disable once ConditionIsAlwaysTrueOrFalseAccordingToNullableAPIContract
+        if (parameterT != null)
+        {
+            var localVariable = parameterT++;
+            
+            Console.WriteLine(localVariable);
+            Console.WriteLine(parameterT.ToString());
+        }
     }
 }
 
@@ -13,7 +25,7 @@ public interface IUsages3<T> where T : IUsages3<T>
 {
     static abstract T AbstractMethod();
 
-    static virtual T? VirtualMethod(T? parameter)
+    static virtual T? VirtualMethod_reabstraction(T? parameter)
     {
         return parameter;
     }
@@ -32,6 +44,34 @@ public interface IUsages3<T> where T : IUsages3<T>
     }
 }
 
+interface ISam1<T> where T : ISam1<T>
+{
+    static abstract T operator +(T a, T b);
+    static abstract implicit operator int(T t);
+}
+
+interface ISam2<T> where T : ISam2<T>
+{
+    static abstract T operator +(T a, T b);
+    static abstract implicit operator int(T t);
+}
+
+//class Foo : ISam1<Foo>, ISam2<Foo>
+//{
+//    // two explicit impls delegating to one sign operator
+//    static Foo ISam1<Foo>.operator +(Foo a, Foo b) => a + b;
+//    static Foo ISam2<Foo>.operator +(Foo a, Foo b) => a + b;
+
+//    public static Foo operator +(Foo a, Foo b) => a;
+
+//    // two explicit impls delegating to one conversion operator
+//    static implicit ISam1<Foo>.operator int(Foo t) => 1;
+//    static implicit ISam2<Foo>.operator int(Foo t) => 2;
+
+//    // CS0557: Duplicate user-defined conversion in type 'Foo'
+//    public static implicit operator int(Foo t) => 42;
+//}
+
 public interface IDerivedUsages3<T> : IUsages3<T> where T : IDerivedUsages3<T>
 {
     static T IUsages3<T>.AbstractMethod()
@@ -39,7 +79,7 @@ public interface IDerivedUsages3<T> : IUsages3<T> where T : IDerivedUsages3<T>
         return default;
     }
 
-    static abstract T? IUsages3<T>.VirtualMethod(T? parameter);
+    static abstract T? IUsages3<T>.VirtualMethod_reabstraction(T? parameter);
 
     /// <summary>
     /// comment
@@ -55,10 +95,9 @@ class DerivedUsages3 : IDerivedUsages3<DerivedUsages3>
 {
     public void Test()
     {
-
     }
 
-    public static DerivedUsages3? VirtualMethod(DerivedUsages3? parameter)
+    public static DerivedUsages3? VirtualMethod_reabstraction(DerivedUsages3? parameter)
     {
         return null;
     }
@@ -66,8 +105,8 @@ class DerivedUsages3 : IDerivedUsages3<DerivedUsages3>
 
 class Usages3_1
 {
-    public void Test()
+    public void Test(DerivedUsages3? derivedUsages3)
     {
-
+        DerivedUsages3.VirtualMethod_reabstraction(derivedUsages3);
     }
 }
